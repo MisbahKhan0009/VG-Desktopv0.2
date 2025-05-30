8f2e85c55f8708e3e9e63062eb7a826c3749f4ee
Misbah Khan <96840923+MisbahKhan0009@users.noreply.github.com>
2025-05-30 17:32:48 +0600

    Fix Tailwind opacity variants for custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ export default {
             accent: {
               red: '#E67E80',
               green: '#A7C080',
-              cyan: '#7FBBB3',
+              cyan: 'rgb(127 187 179 / <alpha-value>)'
             }
           },
           light: {
@@ -25,7 +25,7 @@ export default {
             accent: {
               red: '#F85552',
               green: '#8DA101',
-              cyan: '#3A94C5',
+              cyan: 'rgb(58 148 197 / <alpha-value>)'
             }
           }
         }
